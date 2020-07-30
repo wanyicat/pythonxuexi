6729c3d223288990f424ccb10fa44f1bdf9bd362
huangling <1003822900@qq.com>
2020-07-30 13:14:50 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,21 @@
+# print("hello world!")
+# print(2333)
+# print(False)
+# print("我们老北京"*2)
+# print(1+2+3*(3/2+5*3)) # 可以做计算器用奥
+# print(2>3) #可以做逻辑判断
+# a = "黄岭" #赋予a含义为 黄岭
+# print(a)
+# b = input("请输入:")
+# print("输出的内容：",b)
+# a = float(input("请输入"))
+# b = float(input("请输入"))
+# c = float(input('请输入'))
+# print("输出",a+b+c)
+#len支持的格式有 字符串 数组 元组 字典
+# int是整数 float是小数 str是字符串 bool是布尔值 tuple是元组 list是数组 
+# dict是字典 type是格式 len是计算长度 print是打印 input是输出内容
+# a = input("字符1输入：")
+# b = input("字符2输入：")
+# print("字数：",len(a+b))
+
